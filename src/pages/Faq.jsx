e8008636f1e8e32c7d525c5d4a06d669de78a172
Arinjay Jain <arinjayjain380@gmail.com>
2023-08-18 11:36:09 +0530

    Added accordions to FAQ

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,5 +1,83 @@
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <>
+      <h2 className="text-center p-2">Got questions?</h2>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              How to register?
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              Registering for HacKnight is easy! Just fill the registration form
+              and you are good to go!
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              What is the registration fee?
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              HacKnight is completely free of cost! You don't have to pay
+              anything to participate.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Can I participate from XYZ country?
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              HacKnight is open to all the countries. You can participate from
+              any country in the world.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Faq;
