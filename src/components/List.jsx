cfe67a7078026212039dff4bb78e7731c6640174
Arinjay Jain <arinjayjain380@gmail.com>
2023-08-18 11:03:44 +0530

    Participants Page Completed

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,31 @@
-import PropTypes from 'prop-types';
-import Item from './Item';
+import PropTypes from "prop-types";
+import Item from "./Item";
+import NoteService from "../services/ParticipantService";
+import { useEffect } from "react";
 
-const List = ({ participants }) => {
+const List = ({ participants, getAllParticipants, loading }) => {
+  useEffect(() => {
+    const getParticipants = async () => {
+      try {
+        loading(true);
+
+        const data = await NoteService.getAll();
+        getAllParticipants(data);
+      } catch (error) {
+        window.alert("Something went wrong");
+      } finally {
+        loading(false);
+      }
+    };
+
+    getParticipants();
+  }, []);
+
   return (
     <>
       {participants &&
-        participants.map(participant => (
-          <Item
-            participant={participant}
-            key={participant.id}></Item>
+        participants.map((participant) => (
+          <Item participant={participant} key={participant.id}></Item>
         ))}
     </>
   );
@@ -18,4 +35,6 @@ export default List;
 
 List.propTypes = {
   participants: PropTypes.array,
+  getAllParticipants: PropTypes.func,
+  loading: PropTypes.func,
 };
--- src/pages/Participants.jsx
+++ src/pages/Participants.jsx
@@ -1,15 +1,31 @@
-import List from './../components/List';
-import Empty from './../components/Empty';
-import Loader from '../components/Loader';
+import List from "./../components/List";
+import Empty from "./../components/Empty";
+import Loader from "../components/Loader";
+import { useState } from "react";
 
 const Participants = () => {
+  const [participants, setParticipants] = useState();
+  const [load, setLoad] = useState();
+
+  const getAllParticipants = (participants) => {
+    setParticipants(participants);
+  };
+
+  const loading = (state) => {
+    setLoad(state);
+  };
+
   return (
     <>
       <div className="row">
         <div className="col">
-          <List participants={[]} />
-          <Empty notes={[]} />
-          {false && <Loader />}
+          <List
+            participants={participants}
+            getAllParticipants={getAllParticipants}
+            loading={loading}
+          />
+          <Empty notes={participants} />
+          {load && <Loader />}
         </div>
       </div>
     </>
